ffb4edd983c01a3e9fab8ccccadced910e685704
midhruvjaink <blivefoundations@gmail.com>
2017-07-22 23:45:00 +0100

    mocha test

---
+++ test/index-test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var testing = require('../index');
+describe('tesing', function() {
+    describe('acknowledge function', function() {
+        it('acknowledge test', function () {
+            console.log("successful test!")
+        });
+    });
+});
