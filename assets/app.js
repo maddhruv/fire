eea848cd354d00d9035ca36f27346beed002e1bc
midhruvjaink <blivefoundations@gmail.com>
2017-07-30 23:14:00 +0100

    date and time add

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,21 @@ var myEditor = CodeMirror(document.getElementById("editor"), {
   allowFileDropTypes: ['html', 'css', 'js', 'json', 'py']
 });
 myEditor.setSize("100%", "100%");
+
+//time & date
+function setTime(ts) {
+  var date = new Date(ts);
+  $('.time').text(date.toLocaleString(navigator.language, {
+    hour: '2-digit',
+    minute: '2-digit'
+  })).attr('datetime', date.getHours() + ':' + date.getMinutes());
+  $('.date').text(date.toLocaleString(navigator.language, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })).attr('datetime', date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate());
+  setTimeout(function() {
+    setTime(Date.now());
+  }, 1000);
+}
+setTime(Date.now());
